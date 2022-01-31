2d0b692598ddaad0bf4b6404d2355488f2b97197
jvnsantos <jvnsantos@outlook.com>
2022-01-31 15:37:55 -0300

    feat: implementado metodo GET

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const userController = require("../controllers/User");
+const UserController = require("../controllers/User");
+
+const userController = new UserController();
 
 const router = express.Router();
 
-router.route("/").post(userController.create);
+router.route("/new").post(userController.create);
+router.route("/search").get(userController.findUser);
 
 module.exports = router;
