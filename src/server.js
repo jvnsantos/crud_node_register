8f958f29ceabba41e0fb4710cec22ff502a7d129
jvnsantos <jvnsantos@outlook.com>
2022-02-08 17:55:01 -0300

    feat: add cors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const database = require("./config/database");
 const routes = require("./routes/index.routes");
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 dotenv.config();
 database();
