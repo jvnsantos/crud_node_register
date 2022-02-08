a05a6165ffc484fa1fb74492271986b33a27a18f
jvnsantos <jvnsantos@outlook.com>
2022-02-08 20:29:13 -0300

    feat: CORS fix

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const cors = require("cors")
 const UserController = require("../controllers/UserController");
 
 const userController = new UserController();
 
 const router = express.Router();
 
-router.route("/users/new").post(userController.createUser);
-router.route("/users/search").get(userController.readUser);
-router.route("/users/all").get(userController.readAllUser);
-router.route("/users/delete").delete(userController.deleteUser);
+const whitelist = ["*", "http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+router.route("/users/new").post(userController.createUser, cors(corsOptions));
+router.route("/users/search").get(userController.readUser, cors(corsOptions));
+router.route("/users/all").get(userController.readAllUser, cors(corsOptions));
+router
+  .route("/users/delete")
+  .delete(userController.deleteUser, cors(corsOptions));
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const database = require("./config/database");
 const routes = require("./routes/index.routes");
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
-app.use(cors())
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  app.use(cors());
+  next();
+});
 
 dotenv.config();
 database();
